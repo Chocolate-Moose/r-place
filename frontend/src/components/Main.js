8b8f5e0491c9fde647f83ab86e69c37db574d3dc
Jessica Cheng <jessica.cheng.23@dartmouth.edu>
2022-06-25 19:58:17 -0400

    can change pixel values

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,11 +1,25 @@
 import React, {useState} from "react";
+import { HexColorPicker, HexColorInput } from "react-colorful";
+import Rodal from 'rodal';
 import './main.css';
 import logo from '../assets/logo.png'
+import 'rodal/lib/rodal.css';
 
 export function Main() {
-  const colors = ["#A84FF2", "#F0F24F", "#F2974F", "#F0F24F", "#F0F24F", "#F0F24F", "#F0F24F", "#F0F24F"]
+  const [colors, setShowColors] = useState(Array(9).fill('#cccccc'));
   const [showModal, setShowModal] = useState(false);
-  console.log(showModal)
+  const [activeButton, setActiveButton] = useState(0);
+  const [color, setColor] = useState("");
+
+  const onModalSubmit = () => {
+    setShowModal(false);
+    colors[activeButton] = color;
+  }
+
+  const onPixelClick = (i) => {
+    setShowModal(true); 
+    setActiveButton(i);
+  }
 
   return (
     <div className="container">
@@ -20,11 +34,24 @@ export function Main() {
               className="pixel" 
               key={i} 
               style={{ backgroundColor: `${color}`, cursor: 'pointer'}}
-              onClick={() => setShowModal(true)}
+              onClick={() => onPixelClick(i)}
             />;
         })}
         </div>
       </div>
+      <Rodal visible={showModal} onClose={() => setShowModal(false)} height={500}>
+        <div className="modal-container">
+          <p className="modal-title">Claim This Pixel</p>
+          <div className="color-container">
+            <HexColorPicker color={color} onChange={setColor} />
+            <div className="color-right">
+              <div style={{ backgroundColor: `${color}`, height: '100px', width: '100px'}}/>
+              <HexColorInput color={color} onChange={setColor} />
+            </div>
+          </div>
+          <button onClick={() => onModalSubmit()}>Submit</button>
+        </div>
+      </Rodal>
     </div>
   );
 }
