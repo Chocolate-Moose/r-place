d6bff35cc3e8d034a7f2834ac1bae1445a059339
Jessica Cheng <jessica.cheng.23@dartmouth.edu>
2022-06-26 03:49:38 -0400

    more button stuff

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -20,12 +20,9 @@ export function Main() {
   const [isConnected, setIsConnected] = useState(currentAccount !== "");
   const [isStarted, setIsStarted] = useState(false);
 
-  // useEffect(() => {
-  //   getBalance()
-  // }, [currentBalance]);
-
   const ethers = require("ethers");
   const { Framework } = require("@superfluid-finance/sdk-core");
+  const { defaultAbiCoder } = require("ethers/lib/utils");
 
   const receiver = "0x75825e4F250230375Aa664e456C94eED28737F20"
   const flowRate = 1;
@@ -61,6 +58,10 @@ export function Main() {
     connectWallet();
     setIsConnected(true);
     
+    getBalance()
+  }
+
+  const getBalance = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const sender = provider.getSigner();
     const chainId = await window.ethereum.request({ method: "eth_chainId" });
@@ -74,19 +75,65 @@ export function Main() {
     });
 
     const DAIxContract = await sf.loadSuperToken("fDAIx");
-    getBalance(senderAddress, sender, DAIxContract)
-  }
-
-  const getBalance = async (senderAddress, sender, DAIxContract) => {
     const hi = await DAIxContract.balanceOf({
       account: senderAddress,
       providerOrSigner: sender
     });
-    console.log(hi)
-    setCurrentBalance(`${hi.toString().substring(0, 2)}.${hi.toString().substring(2, 6)}`);
+    setCurrentBalance(`${hi.toString().substring(0, 2)}.${hi.toString().substring(2, 9)}`);
   }
 
+  useEffect(() => {
+    console.log('getting balance')
+    getBalance()
+  }, [currentBalance, getBalance]);
+
   const onStartStream = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const sender = provider.getSigner();
+    const chainId = await window.ethereum.request({ method: "eth_chainId" });
+    const senderAddress = await sender.getAddress();
+
+    const sf = await Framework.create({
+      chainId: Number(chainId),
+      provider: provider,
+      customSubgraphQueriesEndpoint: "",
+      dataMode: "WEB3_ONLY"
+    });
+    const message = defaultAbiCoder.encode(['uint[]', 'string[]'], [[0, 1, 2], ["#FF0000", "#00FF00", "#0000FF"]]);
+
+    const DAIxContract = await sf.loadSuperToken("fDAIx");
+    const DAIx = DAIxContract.address;
+    
+    try {
+      const createFlowOperation = sf.cfaV1.createFlow({
+        receiver: receiver,
+        flowRate: flowRate,
+        superToken: DAIx,
+        userData: `${message}`
+      });
+  
+      console.log("Creating your stream...");
+  
+      const result = await createFlowOperation.exec(sender);
+      console.log(result);
+      console.log(defaultAbiCoder.decode(['uint[]', 'string[]'], message))
+  
+      console.log(
+        `Congrats - you've just created a money stream!
+      View Your Stream At: https://app.superfluid.finance/dashboard/${receiver}
+      Chain ID: ${chainId}
+      Super Token: DAIx
+      Sender: ${senderAddress}
+      Receiver: ${receiver}
+      FlowRate: ${flowRate}
+      `
+      );
+    } catch (error) {
+      console.log(
+        "Hmmm, your transaction threw an error. Make sure that this stream does not already exist, and that you've entered a valid Ethereum address!"
+      );
+      console.error(error);
+    }
     setIsStarted(true);
   }
 
@@ -103,7 +150,7 @@ export function Main() {
         </div>
         <div className="header-right">
         
-          {isConnected ? <p className = "wallet-balance">{`${currentAccount.substring(0, 4)}...${currentAccount.substring(38)}`}</p> : null}
+          {isConnected ? <p className = "wallet-balance">{`${currentAccount.substring(0, 4)}...${currentAccount.substring(39)}`}</p> : null}
           {isConnected ? <p className = "wallet-balance">{currentBalance}</p> : null}
           <div className="header-button-container">
             {!isConnected ? <button className="connect-button" onClick={() => onConnectWallet()}>Connect Wallet</button> : null}
